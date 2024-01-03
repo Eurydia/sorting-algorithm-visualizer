900bd0a42da650a7c2a68a467c5b18e33ab882e1
Euryn <crossdeltavi@gmail.com>
2024-01-03 08:17:05 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), tsconfigPaths()],
-	base: "https://eurydia.github.io/sorting-algorithm-visualizer/",
+	base: "https://eurydia.github.io/project-sorting-algorithm-visualizer/",
 });
